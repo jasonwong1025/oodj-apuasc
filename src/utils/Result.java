9b945a03c636d4f74cf1f0bb95bff3bafc033bfa
ryanlau1220 <liujunhong20@gmail.com>
2026-04-09 00:22:42 +0800

    add result object to allow ui to catch all specific validation

---
+++ src/utils/Result.java
@@ -0,0 +1,36 @@
+package utils;
+
+/**
+ * A simple container for service layer responses.
+ * Can carry a success value of type T or a failure error message.
+ */
+public final class Result<T> {
+
+    private final T value;
+    private final String error;
+
+    private Result(T value, String error) {
+        this.value = value;
+        this.error = error;
+    }
+
+    public static <T> Result<T> success(T value) {
+        return new Result<>(value, null);
+    }
+
+    public static <T> Result<T> failure(String error) {
+        return new Result<>(null, error);
+    }
+
+    public boolean isSuccess() {
+        return error == null;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public String getError() {
+        return error;
+    }
+}
