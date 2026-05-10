9936e67f9ed495ae15590e3d3a454a9fe79f1024
ryanlau1220 <liujunhong20@gmail.com>
2026-05-10 20:16:53 +0800

    move Refreshable to core package with new PortalContext interface

---
+++ src/ui/core/PortalContext.java
@@ -0,0 +1,21 @@
+package ui.core;
+
+import abstracts.AbstractUser;
+import service_layer.*;
+import javax.swing.JFrame;
+
+/**
+ * Shared interface for all portal contexts, providing access to services and shared state.
+ */
+public interface PortalContext {
+    JFrame owner();
+    AbstractUser currentUser();
+    UserService userService();
+    AppointmentService appointmentService();
+    PaymentService paymentService();
+    ReviewService reviewService();
+    VehicleService vehicleService();
+    ServiceService serviceService();
+    RegistrationService registrationService();
+    Runnable refreshAction();
+}
---
+++ src/ui/core/Refreshable.java
@@ -0,0 +1,8 @@
+package ui.core;
+
+/**
+ * Interface for UI components that can be refreshed.
+ */
+public interface Refreshable {
+    void refresh();
+}
